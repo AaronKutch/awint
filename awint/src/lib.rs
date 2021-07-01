9c421eb3eef4d8c96ba3d2cc84040ec44ed3dbc9
Aaron Kutch <aaronkutch@att.net>
2021-07-01 17:20:35 -0500

    Remove awint_dag support for now

--- awint/src/lib.rs
+++ awint/src/lib.rs
@@ -4,8 +4,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub use awint_core::{bw, Bits, InlAwi, SerdeError};
-#[cfg(feature = "awint_dag")]
-pub use awint_dag;
+//#[cfg(feature = "awint_dag")]
+//pub use awint_dag;
 #[cfg(feature = "alloc")]
 pub use awint_ext::ExtAwi;
 pub use awint_macros::*;
@@ -20,9 +20,11 @@ pub mod prelude {
     pub use crate::{bw, cc, Bits, InlAwi};
 }
 
+/*
 #[cfg(feature = "awint_dag")]
 pub mod dag_prelude {
     pub use awint_core::bw;
     pub use awint_dag::{Bits, ExtAwi, InlAwi};
     pub use awint_macros::*;
 }
+*/
